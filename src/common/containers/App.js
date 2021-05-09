7b4c7688b53f0305291ccad8d96aac14668f9ee0
Kurt Lopez <kurtdevph@gmail.com>
2021-05-09 15:17:38 +0800

    add dark mode functionality

--- src/common/containers/App.js
+++ src/common/containers/App.js
@@ -1,5 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Theme from '../../context/theme-context';
 
 export default function App({ children }) {
-  return children;
+  const [isDark, setIsDark] = useState(false);
+
+  const toggleTheme = () => {
+    setIsDark(!isDark);
+  };
+
+  useEffect(() => {
+    document.querySelector('html').classList.toggle('dark-mode', isDark);
+  }, [isDark]);
+  return (
+    <Theme.Provider value={{ isDark, toggleTheme }}>{children}</Theme.Provider>
+  );
 }
